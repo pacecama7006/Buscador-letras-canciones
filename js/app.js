4859bb3965cb3e6026ccf8b82adf1840a27c0b70
Paulo <pacecama7006@gmail.com>
2022-08-20 03:23:58 -0500

    Se crea la funcionalidad para mostrar un mensaje de error en caso de que los usuarios no llenen los dos campos del formulario. Se trabaja con modulos, por lo cual, en el index.html se pone el type module y la clase interfaz se crean los elementos del formulario que van a ser exportados por él e importados por los otros archivos.

---
+++ js/app.js
@@ -0,0 +1,38 @@
+import * as UI from "./interfaz.js";
+// console.log(UI);
+
+UI.formularioBuscar.addEventListener('submit', buscarCancion);
+
+/**Función que me permite buscar una canción en la api 
+ * https://lyricsovh.docs.apiary.io/#reference/0/lyrics-of-a-song/search
+ */
+function buscarCancion(e) {
+    e.preventDefault();
+
+    // Obtener datos del formulario
+    /**Ojo: no puedo utilizar los campos del formulario de esta forma
+     * los tengo que crear en este archivo, porque sino, no lee
+     * la información de los inputs y me genera un error
+     * POr eso los comento en iterfaz y los genero aquí
+     */
+    // UI.artista.value;
+    // UI.cancion.value;
+    const artista = document.querySelector('#artista').value;
+    const cancion = document.querySelector('#cancion').value;
+
+    // Valido datos del formulario
+    if (artista === '' || cancion === '') {
+        // El usuario dejó al menos un campo vacío. Mostrar error
+        UI.divMensajes.textContent = 'Error.. Todos los campos son obligatorios';
+        UI.divMensajes.classList.add('error');
+
+        setTimeout(() => {
+            UI.divMensajes.textContent= '';
+            UI.divMensajes.classList.remove('error');
+        }, 3000);
+
+        return
+    }
+
+    // Consultamos la api
+}
---
+++ js/interfaz.js
@@ -0,0 +1,8 @@
+// Variables del html
+export const formularioBuscar = document.querySelector('#formulario-buscar');
+// export const artista = document.querySelector('#artista');
+// export const cancion = document.querySelector('#cancion');
+export const divBuscar = document.querySelector('#buscar');
+export const divMensajes = document.querySelector('#mensajes');
+export const divResultado = document.querySelector('#resultado');
+export const headingResultado = document.querySelector('.letra-resultado h2');
